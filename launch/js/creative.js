81386b8ca221e6fc13abd2fbdc3bafea844c8457
sangaviselvaraj <sangaviselvaraj5@gmail.com>
2016-08-10 18:10:31 +0530

    new34

--- launch/js/creative.js
+++ launch/js/creative.js
@@ -47,6 +47,8 @@ $('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$
         $('.scrollpoint.sp-effect3').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDown');},{offset:'100%'});
         $('.scrollpoint.sp-effect4').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeIn');},{offset:'100%'});
         $('.scrollpoint.sp-effect5').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInUp');},{offset:'100%'});
+         $('.scrollpoint.sp-effect6').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDownBig');},{offset:'100%'});
+
 
        
         });
