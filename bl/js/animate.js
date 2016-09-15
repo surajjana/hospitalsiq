573c9f17aaf99d94b5ec1e377f2dac3855f43e23
sangaviselvaraj <sangaviselvaraj5@gmail.com>
2016-09-15 15:16:51 +0530

    new51

--- bl/js/animate.js
+++ bl/js/animate.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 $('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInLeft');},{offset:'100%'});
         $('.scrollpoint.sp-effect2').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInRight');},{offset:'100%'});
        
-
+ $('.scrollpoint.sp-effect5').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInUp');},{offset:'100%'});
 
        
         });
