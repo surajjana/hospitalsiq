214b9d4c40372e4c04618ac4954f310b1b29ac67
sangaviselvaraj <sangaviselvaraj5@gmail.com>
2016-09-19 15:24:00 +0530

    check1

--- sangu/js/animation.js
+++ sangu/js/animation.js
@@ -5,11 +5,8 @@
 		botMouth = animatingSvg.select('#botmouth'),
 		talkBox = animatingSvg.select('#talkbox'),
 		botText = animatingSvg.select('#hello');
-	var animatingSvg = Snap('#cd-animated-svg'),
-	
-		textQ = animatingSvg.select('#text-q'),
-		textPlus = animatingSvg.select('#text-p'),
-		logo = animatingSvg.select('#logo');
+
+
 	initLoading();	
 
 	function initLoading() {
@@ -21,7 +18,7 @@
 
         window.setInterval(animateStars,4000);
 		animateFace();
-			animateQ();
+		
 
 	}
 
@@ -129,51 +126,6 @@
 		},2000);
 	}
 
-
-	function animateQ(){
-		document.getElementById('text-q').style.display = "block";
-		textQ.addClass('path-q');
-		setTimeout(function(){
-			textQ.addClass('fill-q animated wobble');
-			
-		}, 1000);
-		setTimeout(function () {
-			animatePlus();
-		}, 2000);
-	}
-
-	function animatePlus(){
-		document.getElementById('text-q').style.display = "none";
-		textQ.removeClass('fill-q animated wobble');
-		document.getElementById('text-p').style.display = "block";
-		textPlus.addClass('plus animated zoomIn');
-		setTimeout(function(){
-			textPlus.removeClass('animated');
-			textPlus.removeClass('zoomIn');
-		}, 1500);
-		setTimeout(function(){
-			textPlus.addClass('fade-out');
-		}, 1500);
-		setTimeout(function(){
-			document.getElementById('text-p').style.display = "none";
-			animateLogo();
-		}, 2500);
-	}
-
-	function animateLogo(){
-		document.getElementById('logo').style.display = "block";
-		logo.addClass('fade-in');
-		setTimeout(function(){
-			logo.removeClass('fade-in');
-		}, 1500);
-		setTimeout(function(){
-			logo.addClass('fade-out');
-		}, 2000);
-		setTimeout(function(){
-			document.getElementById('logo').style.display = "none";
-			animateQ();
-		}, 2500);
-	}
 
 
 })();
